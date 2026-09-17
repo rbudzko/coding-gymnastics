267886b1fcea1f4973693bd62e2bd9c704c6fd45
Robert Budźko <robert.krzysztof.budzko@gmail.com>
2026-09-17 16:53:23 +0200

    Add: find-minimum-in-rotated-sorted-array - submission-3

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left=0
+        right=len(nums)-1
+
+        while left <= right:
+            mid=(right+left)//2
+            
+            if nums[mid] <= nums[mid-1]:
+                return nums[mid]
+            elif nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
